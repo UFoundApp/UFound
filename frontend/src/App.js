cb8db9601c54138956eefd36f1a36647e1e4a4b9
Yousef Abdelhadi <mmax20920@gmail.com>
2025-02-02 18:57:14 -0500

    BUTM-11: Added main page with navbar and sidebars

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,24 @@ import AuthPage from "./components/AuthPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/home";
-
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { system } from './theme';  // Your custom minimal theme
+import MainPage from './MainPage';
 
 function App() {
   return (
+    
     <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<AuthPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <ChakraProvider value={system}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </ChakraProvider>
     </Routes>
   </Router>
   );
 }
 
 export default App;
-
---
+++ frontend/src/components/Timeline.js
@@ -0,0 +1,14 @@
+// Timeline.js
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const Timeline = () => {
+    return (
+        <Box p={4} bg="white">
+            {/* Placeholder for timeline posts */}
+            Timeline (posts will be displayed here)
+        </Box>
+    );
+};
+
+export default Timeline;
