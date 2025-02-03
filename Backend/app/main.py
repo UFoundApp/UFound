88e0a496c646e32477ee9e61cf6d895ffffc6427
amaan <amaan.farooqi@mail.utoronto.ca>
2025-02-02 23:58:44 -0500

    BUTM15: Created post details page with basic comment and like buttons and section

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,12 +1,37 @@
 from fastapi import FastAPI
-from app.db import test_connection
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.db import test_connection, db
+from app.models.posts import PostModel, CommentModel  # Ensure correct import
+from app.models.user import UserModel
+from app.routes.posts import router as post_router 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # ✅ Allows React frontend to communicate
+    allow_credentials=True,
+    allow_methods=["*"],  # ✅ Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # ✅ Allows all headers
+)
+
+
+async def init_db():
+    await init_beanie(db, document_models=[PostModel, UserModel, CommentModel])  # Register discussion model
 
 @app.on_event("startup")
 async def startup():
     await test_connection()
+    await init_db()
+ 
+app.include_router(post_router, prefix="/api", tags=["Posts"])
 
 @app.get("/")
 async def root():
-    return {"message": "UTM Blinds API is running!"}
+    return {"message": "UFound API is running!"}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+import ViewPost from './Posts/ViewPost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Routes>
+          <Route path="/view-post/:id" element={<ViewPost/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
