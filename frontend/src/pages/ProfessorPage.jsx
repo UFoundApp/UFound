842b53b1bc1f887f6f8c946525f8a7faf3259548
Jason Li <Jasonli3566@gmail.com>
2025-03-29 18:15:07 -0400

    bug fix

--- frontend/src/pages/ProfessorPage.jsx
+++ frontend/src/pages/ProfessorPage.jsx
@@ -105,7 +105,7 @@ const ProfessorPage = () => {
             setUser(u);
         };
         loadUser();
-        
+        if (!professorId) return; 
         fetch(`http://127.0.0.1:8000/api/professors/${professorId}/page`)
             .then((response) => response.json())
             .then((data) => {
